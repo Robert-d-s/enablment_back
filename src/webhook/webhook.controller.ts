557486a41572846ac198bf8ff0b596a9ef51ee61
SamAtMedly <sh@medley.vc>
2023-05-08 08:56:16 +0200

    fix webhook auth

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
 import { WebhookGuard } from './webhook.guard';
+import { Public } from 'src/auth/auth.controller';
 
 @Controller('webhook')
 export class WebhookController {
@@ -8,6 +9,7 @@ export class WebhookController {
     constructor(private webhookService: WebhookService) {}
 
     @Post()
+    @Public()
     @UseGuards(WebhookGuard)
     async handle(@Body() body: any) {
         await this.webhookService.handle(body)
