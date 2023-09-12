87ea06621a7ff149492275c022cdbaf9b4d7f9e6
Robert-d-s <110900093+Robert-d-s@users.noreply.github.com>
2023-09-12 11:11:36 +0200

    cors update and env

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://3d3d-152-115-126-50.ngrok-free.app',
+    ],
   });
 
   await app.listen(process.env.PORT || 8080);
