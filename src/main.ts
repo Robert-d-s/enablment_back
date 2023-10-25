a4a331904e807304c20a807c250f970cd37a8e8e
Robert-d-s <110900093+Robert-d-s@users.noreply.github.com>
2023-10-25 11:14:32 +0200

    ..

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://a869-152-115-126-50.ngrok-free.app ',
+      'https://fb64-213-32-242-65.ngrok-free.app ',
     ],
   });
 
