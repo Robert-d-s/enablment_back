e54a8add4684da59a48a65b4f53a1104e37baf14
Robert-d-s <110900093+Robert-d-s@users.noreply.github.com>
2023-12-15 14:46:05 +0100

    ..

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
+      'https://enablment-ouh24tctl-robert-d-s.vercel.app',
     ],
   });
 
