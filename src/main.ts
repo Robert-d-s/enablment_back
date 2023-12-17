52fd99ce04544e81cfdb7b3f29bfe0ab22269c78
Robert-d-s <110900093+Robert-d-s@users.noreply.github.com>
2023-12-17 23:55:28 +0100

    ..

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: [
-  //     'http://localhost:3000',
-  //     'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
-  //     'https://enablment-tt.vercel.app/',
-  //   ],
-  //   credentials: true,
-  //   allowedHeaders: ['Content-Type', 'Authorization'],
-  // });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
+      'https://enablment-tt.vercel.app/',
+    ],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
