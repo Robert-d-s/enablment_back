6e228ae207da173e0cd817d43bca38f83deb47f8
Robert-d-s <110900093+Robert-d-s@users.noreply.github.com>
2023-12-17 23:26:21 +0100

    ..

--- src/main.ts
+++ src/main.ts
@@ -13,17 +13,26 @@ async function bootstrap() {
   //   credentials: true,
   //   allowedHeaders: ['Content-Type', 'Authorization'],
   // });
-  app.use(
-    cors({
-      origin: [
-        'http://localhost:3000',
-        'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
-        'https://enablment-tt.vercel.app/',
-      ],
-      credentials: true,
-      preflightContinue: true,
-    }),
-  );
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'http://localhost:3000',
+      'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
+      'https://enablment-tt.vercel.app',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    if (req.method === 'OPTIONS') {
+      res.header(
+        'Access-Control-Allow-Methods',
+        'PUT, POST, PATCH, DELETE, GET',
+      );
+      return res.status(200).json({});
+    }
+    next();
+  });
 
   await app.listen(process.env.PORT || 8080);
 }
