d9786cfe1ca51f34a585525c11a55c26a032d626
Robert-d-s <110900093+Robert-d-s@users.noreply.github.com>
2023-12-18 00:01:59 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,41 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
-      'https://enablment-tt.vercel.app/',
-    ],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    if (req.method === 'OPTIONS') {
-      res.header(
-        'Access-Control-Allow-Methods',
-        'PUT, POST, PATCH, DELETE, GET',
-      );
-      return res.status(200).json({});
-    }
-    next();
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
+  //     'https://enablment-tt.vercel.app/',
+  //   ],
+  //   credentials: true,
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header(
+  //     'Access-Control-Allow-Headers',
+  //     'Origin, X-Requested-With, Content-Type, Accept',
+  //   );
+  //   if (req.method === 'OPTIONS') {
+  //     res.header(
+  //       'Access-Control-Allow-Methods',
+  //       'PUT, POST, PATCH, DELETE, GET',
+  //     );
+  //     return res.status(200).json({});
+  //   }
+  //   next();
+  // });
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:3000',
+        'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
+        'https://enablment-tt.vercel.app',
+      ],
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+  );
 
   await app.listen(process.env.PORT || 8080);
 }
