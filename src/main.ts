e54885193e9170c96c0b1b23e316673f839b930b
Robert-d-s <110900093+Robert-d-s@users.noreply.github.com>
2023-12-17 23:13:51 +0100

    trying to fix cors issue

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
-      'https://enablment-tt.vercel.app/',
-    ],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
+  //     'https://enablment-tt.vercel.app/',
+  //   ],
+  //   credentials: true,
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:3000',
+        'https://52d3-2a02-2f08-d106-c300-70dd-50f2-5394-12aa.ngrok-free.app',
+        'https://enablment-tt.vercel.app/',
+      ],
+      credentials: true,
+    }),
+  );
 
   await app.listen(process.env.PORT || 8080);
 }
